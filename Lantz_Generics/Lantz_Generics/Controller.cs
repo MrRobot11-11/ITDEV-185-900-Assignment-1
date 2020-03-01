c4450e50e38be79583ffca479eccb3e6b0ba0354
Steven Lantz <47541193+MrRobot11-11@users.noreply.github.com>
2020-03-01 15:37:54 -0600

    Add files via upload

---
+++ Lantz_Generics/Lantz_Generics/Controller.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lantz_Generics
+{
+
+    
+    class Controller
+    {
+        View vw = new View();
+
+      public void RunProgram()
+        {
+            vw.Intro();
+            
+            do
+            {
+                Console.ReadLine();
+                Console.Clear();
+
+
+
+                vw.Menu();
+
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    MyGeneric<int> compare = new MyGeneric<int>();
+                    compare.CompareTwoValues(int.Parse(vw.GetFirstValueString()), int.Parse(vw.GetSecondValueString()));
+                }
+
+                if (input == "2")
+                {
+                    MyGeneric<string> compare2 = new MyGeneric<string>();
+                    compare2.CompareTwoValues(vw.GetFirstValueString(), vw.GetSecondValueString());
+                }
+
+                
+            } while (!vw.MoreComparisons());
+
+      
+        }//End of RunProgram
+
+      
+        
+    }//End of class
+    
+}//End of namespace
---
+++ Lantz_Generics/Lantz_Generics/MyGeneric.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lantz_Generics
+{
+    class MyGeneric<T> where T: IComparable
+    {
+
+        
+
+        public bool CompareTwoValues<T>(T value1, T value2)
+
+        {
+            if (EqualityComparer<T>.Default.Equals(value1, value2))
+            {
+                Console.WriteLine("\nThe two values are equal!");
+                return true;
+            }
+
+            else
+            {
+                Console.WriteLine("\nThe values are not equal.");
+                return false;
+            }
+                
+        }
+
+     
+      
+    }
+}
